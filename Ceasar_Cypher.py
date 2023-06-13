2854379ae1fac1b06e08d4ffa7ee84eaae0033d4
Utkarsh0906 <96988514+Utkarsh0906@users.noreply.github.com>
2023-06-13 11:46:51 +0530

    Update Ceasar_Cypher.py

--- Ceasar_Cypher.py
+++ Ceasar_Cypher.py
@@ -1,8 +1,11 @@
 from Alphabet import alphabet 
+
 def cypher(code, shift,direction):
+    
     if(direction == "d"):
         shift *= -1
     code = list(code)
+    
     for i in range (len(code)):
         if code[i] in alphabet:
             index = alphabet.index(code[i])+shift
@@ -10,10 +13,12 @@ def cypher(code, shift,direction):
     return "".join(code)
 
 while(True):
+    
     direction = input("Enter E for encoding and D for decoding: ").lower()
     code = input("Enter the string: ").lower()
     shift = int(input("Enter the shift: "))
     print(cypher(code,shift,direction))
     call = input("Enter Y to continue and N to stop: ").lower()
+    
     if(call == "n"):
         break
