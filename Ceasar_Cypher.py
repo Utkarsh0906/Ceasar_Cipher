d90dcf31d0b7dea433e9942c9dfdb9502d7f6ad9
Utkarsh0906 <96988514+Utkarsh0906@users.noreply.github.com>
2023-06-13 11:38:36 +0530

    Update Ceasar_Cypher.py

--- Ceasar_Cypher.py
+++ Ceasar_Cypher.py
@@ -1,30 +1,18 @@
 from Alphabet import alphabet 
-def encode(code, shift):
+def cypher(code, shift,direction):
+    if(direction == "d"):
+        shift *= -1
     code = list(code)
     for i in range (len(code)):
         index = alphabet.index(code[i])+shift
-        if index>25:
-            index -=26
         code[i] = alphabet[index]
-    return "".join(code)
-def decode(code, shift):
-    code = list(code)
-    for i in range (len(code)):
-        code[i] = alphabet[alphabet.index(code[i])-shift]
     return "".join(code)
 
 while(True):
-    call = input("Enter E for encoding and D for decoding: ").lower()
-    if(call == "e"):
-        code = input("Enter the string to encode: ").lower()
-        shift = int(input("Enter the shift: "))
-        print(encode(code,shift))
-    elif(call == "d"):
-        code = input("Enter the string to decode: ").lower()
-        shift = int(input("Enter the shift: "))
-        print(decode(code,shift))
-    else:
-        print("Invalid input")
+    direction = input("Enter E for encoding and D for decoding: ").lower()
+    code = input("Enter the string to encode: ").lower()
+    shift = int(input("Enter the shift: "))
+    print(cypher(code,shift,direction))
     call = input("Enter Y to continue and N to stop: ").lower()
-    if(call == "N"):
+    if(call == "n"):
         break
